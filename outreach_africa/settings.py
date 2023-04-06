615232d657fd79ac05390f751db0e8c13a9c669c
Michaelizzy1 <michaelizzy2@gmail.com>
2023-04-06 21:20:17 +0100

    database

--- outreach_africa/settings.py
+++ outreach_africa/settings.py
@@ -81,19 +81,19 @@ WSGI_APPLICATION = 'outreach_africa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# import dj_database_url
-#
 # DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 
 # Password validation
