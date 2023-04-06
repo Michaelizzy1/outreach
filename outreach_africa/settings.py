a73b7ddbac8b0953bbb3ce08e9211ef3f9f7b31b
Michaelizzy1 <michaelizzy2@gmail.com>
2023-04-06 23:51:35 +0100

    images

--- outreach_africa/settings.py
+++ outreach_africa/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
